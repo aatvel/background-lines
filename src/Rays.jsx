a02eb6306206d99cb5ec640d6737e04ee8763fc1
Alina <alina.andrevna3993@gmail.com>
2023-08-10 17:23:30 +0300

    random scale

--- src/Rays.jsx
+++ src/Rays.jsx
@@ -8,6 +8,8 @@ import { gsap } from "gsap";
 
 export const Rays = () => {
   const ref = useRef();
+  const clones = useRef([]);
+  const clonesDown = useRef([]);
   const model = useGLTF("/box.glb");
   const { aNumber, bNumber, cNumber, nNumber } = useControls({
     nNumber: {
@@ -16,32 +18,31 @@ export const Rays = () => {
       max: 5,
       step: 0.01,
     },
-    // aNumber: {
-    //   value: 0.8,
-    //   min: 0.0,
-    //   max: 1.2,
-    //   step: 0.01,
-    // },
-    // bNumber: {
-    //   value: 0.8,
-    //   min: 0.0,
-    //   max: 1.2,
-    //   step: 0.01,
-    // },
-    // cNumber: {
-    //   value: 0.8,
-    //   min: 0.0,
-    //   max: 1.2,
-    //   step: 0.01,
-    // },
   });
 
+
+  useEffect(() => {
+    clones.current.forEach((clone) => {
+      gsap.to(clone.scale, {
+        y: 1.6 * nNumber * Math.random(),
+        duration: 0.5,
+        ease: "power2.out",
+      });
+    });
+    clonesDown.current.forEach((clone) => {
+      gsap.to(clone.scale, {
+        y: - 1.6 * nNumber * Math.random(),
+        duration: 0.5,
+        ease: "power2.out",
+      });
+    });
+  }, [nNumber]);
 
   return (
     <>
       <Clone
-        ref={ref}
         object={model.scene}
+        ref={(node) => (clones.current[0] = node)}
         position={[-2.1, -0.3, -3]}
         scale={[0.009, 1.6 * nNumber, 0.009]}
         rotation={[
@@ -52,6 +53,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[1] = node)}
         position={[-1.8, -0.29, -3]}
         scale={[0.009, 1.6 * nNumber, 0.009]}
         rotation={[
@@ -62,6 +64,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[2] = node)}
         position={[-1.5, -0.28, -3]}
         scale={[0.009, 1.6 * nNumber, 0.009]}
         rotation={[
@@ -72,6 +75,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[3] = node)}
         position={[-1.2, -0.27, -3]}
         scale={[0.009, 1.6 * nNumber, 0.009]}
         rotation={[
@@ -82,6 +86,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[4] = node)}
         position={[-0.9, - 0.26, -3]}
         scale={[0.009, 1.6* nNumber, 0.009]}
         rotation={[
@@ -92,6 +97,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[5] = node)}
         position={[-0.6, -0.25, -3]}
         scale={[0.009, 1.6* nNumber, 0.009]}
         rotation={[
@@ -102,6 +108,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[6] = node)}
         position={[ - 0.3, -0.26, -3]}
         scale={[0.009, 1.6* nNumber, 0.009]}
         rotation={[
@@ -112,6 +119,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[7] = node)}
         position={[0, -0.27, -3]}
         scale={[0.009, 1.6* nNumber, 0.009]}
         rotation={[
@@ -122,6 +130,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[8] = node)}
         position={[0.3, -0.28, -3]}
         scale={[0.009, 1.6* nNumber, 0.009]}
         rotation={[
@@ -132,6 +141,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[9] = node)}
         position={[0.6, -0.29, -3]}
         scale={[0.009, 1.6* nNumber, 0.009]}
         rotation={[
@@ -142,6 +152,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clones.current[10] = node)}
         position={[0.9, -0.3, -3]}
         scale={[0.009, 1.6* nNumber, 0.009]}
         rotation={[
@@ -157,6 +168,7 @@ export const Rays = () => {
 
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[0] = node)}
         position={[-2.1, -0.5, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -167,6 +179,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[1] = node)}
         position={[-1.8, -0.51, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -177,6 +190,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[2] = node)}
         position={[-1.5, -0.52, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -187,6 +201,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[3] = node)}
         position={[-1.2, -0.53, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -197,6 +212,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[4] = node)}
         position={[-0.9, -0.54, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -207,6 +223,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[5] = node)}
         position={[-0.6, -0.55, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -217,6 +234,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[6] = node)}
         position={[-0.3, -0.56, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -227,6 +245,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[7] = node)}
         position={[0, -0.55, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -237,6 +256,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[8] = node)}
         position={[0.3, -0.54, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -247,6 +267,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[9] = node)}
         position={[0.6, -0.53, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
@@ -257,6 +278,7 @@ export const Rays = () => {
       />
       <Clone
         object={model.scene}
+        ref={(node) => (clonesDown.current[10] = node)}
         position={[0.9, -0.52, -3]}
         scale={[0.009, -1.6* nNumber, 0.009]}
         rotation={[
